b74b8603b0f1c8461a2bc1b6bfb78b56e3484b43
Abdoelabassi <abderrezaq@outlook.com>
2022-06-04 19:57:23 +0100

    success

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextFetchEvent, NextRequest, NextResponse} from "next/server"
 import {nanoid} from "nanoid";
 
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
     if(req.nextUrl.pathname.startsWith("/api/get-url")) {
 
@@ -11,6 +11,22 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
         
         return;
     }
+
+    
+
+    const slug = req.nextUrl.pathname.split("/").pop();
+     
+    console.log(slug)
+
+
+    const data = await (
+        await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)).json();
+
+
+    if(data?.url){
+        return NextResponse.redirect(data.url);
+        }
+    
 
 
 
